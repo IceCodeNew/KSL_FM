2a0c07677ca1902b552f3be2b45d3a9db4c6e8ab
goodbest <lovegoodbest@gmail.com>
2014-11-28 09:36:10 +0800

    add playlist and song cache

--- player.php
+++ player.php
@@ -1,22 +1,41 @@
 <?php
 include 'api.php';
 include 'list.php';
+
 if(($_GET['album']) != '')
 	$playlist_list =array($playlist_list[$_GET['album']]);
 
-foreach ($playlist_list as $key) {
-    $json = get_playlist_info($key);
-    $arr = json_decode($json, true);
+foreach ($playlist_list as $key => $value) {
+	if(file_exists('playlist_cache/'.$value.'.json')){
+		$arr=json_decode(file_get_contents('playlist_cache/'.$value.'.json'),true);
+	}
+	else{
+    	$json = get_playlist_info($value);
+    	$arr = json_decode($json, true);
+    	file_put_contents('playlist_cache/'.$value.'.json', json_encode($arr));
+    }
+    if(!in_array($key, $playlist_cache)){
+    	$playlist_cache[$key]=$arr;
+    }
+
     foreach ($arr["result"]["tracks"] as $key2) {
         $id = $key2["id"];
         if (!in_array($id, $player_list)) {
             $player_list[] = $id;
+            #$song_cache[$id]=$key2;
         }
     }
 }
 
+
 $id = get_music_id();
-$music_info = json_decode(get_music_info($id), true);
+if(file_exists('song_cache/'.$id.'.json')){
+	$music_info=json_decode(file_get_contents('song_cache/'.$id.'.json'),true);
+}
+else{
+	$music_info = json_decode(get_music_info($id), true);
+	file_put_contents('song_cache/'.$id.'.json', json_encode($music_info));
+}
 #echo json_encode($music_info);
 $play_info["cover"] = $music_info["songs"][0]["album"]["picUrl"];
 $play_info["mp3"] = $music_info["songs"][0]["mp3Url"];
